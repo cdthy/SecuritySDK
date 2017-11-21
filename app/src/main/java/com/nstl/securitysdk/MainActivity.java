d56a7ef6075cf383fd47ac5a99e923dc325418af
ffthy <ffthy@qq.com>
2017-11-21 21:39:17 +0800

    新增了签名校验和hooking检测

--- app/src/main/java/com/nstl/securitysdk/MainActivity.java
+++ app/src/main/java/com/nstl/securitysdk/MainActivity.java
@@ -2,15 +2,19 @@ package com.nstl.securitysdk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.nstl.securitysdkcore.HelpUtil;
+import com.nstl.securitysdkcore.NativeCoreUtil;
 import com.nstl.securitysdkcore.reinforce.DetectRootUtil;
+import com.nstl.securitysdkcore.reinforce.IVerifyListener;
 import com.nstl.securitysdkcore.reinforce.bean.InstallPackageInfo;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MaintActivity";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        TextView tv = (TextView) findViewById(R.id.root_id);
         //tv.setText("hello“");
         /*HelpUtil helpUtil = new HelpUtil();
         List<InstallPackageInfo> installPackageInfoList = helpUtil.getInstallPackageAndSig(this);
@@ -33,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
         }*/
         DetectRootUtil detectRootUtil = DetectRootUtil.getInstance(this);
         tv.setText("设备是否：" + detectRootUtil.isRoot());
+        final  TextView tv1=(TextView) findViewById(R.id.checkSign_id);
+        NativeCoreUtil nativeCoreUtil=new NativeCoreUtil();
+        nativeCoreUtil.rePackage(MainActivity.this, new IVerifyListener() {
+            @Override
+            public void onVerifySuccess() {
+                tv1.setText("签名校验通过");
+                Log.d(TAG, "onVerifySuccess: 签名没问题");
+            }
+
+            @Override
+            public void onVerifyFail() {
+                tv1.setText("签名校验不通过");
+                Log.d(TAG, "onVerifyFail: 签名出问题了");
+            }
+        });
+        final  TextView tv2=(TextView) findViewById(R.id.InjectDetected_id);
+        int flag=nativeCoreUtil.detectInject(MainActivity.this);
+        if(flag==-1){
+            tv2.setText("未发现设备被注入");
+        };
+        if (flag==1){
+            tv2.setText("设备被注入");
+        }
     }
 
     /**
